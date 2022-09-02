68a23b47802d6b3d23456f7bf050ca0d8605bf86
fbambague <dbambague7@gmail.com>
2022-09-01 22:10:12 -0500

    Creación myStore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent = '';
+  showImg = true;
+
+  //https://media.istockphoto.com/photos/anfield-stadium-the-home-ground-of-liverpool-fc-picture-id1032395842
+
+  onLoaded(img: string){
+    console.log('log padre', img);
+  }
+
+  toggleImg(){
+    this.showImg = !this.showImg;
+  }
+
 }
